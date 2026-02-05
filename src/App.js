f63f4dde3a296f225d5ac498003570ab4061b520
Miskachev Dima <miskachevds@yandex.ru>
2026-02-05 16:31:27 +0300

    components,scss,props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './scss/app.scss'
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+
+        {/* <div className="block1">
+          <div className="text2">
+            <ul className="text2">
+              <li className="hello3">Test1</li>
+              <li className="hello3">Test1</li>
+              <li className="hello3">Test1</li>
+              <li className="hello3">Test1</li>
+              <li className="hello3">Test1</li>
+            </ul>
+          </div>
+        </div> */}
+
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title='Мексиканчкая' price={500} />
+            <PizzaBlock title='hello' price='399'/>
+            <PizzaBlock />
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
