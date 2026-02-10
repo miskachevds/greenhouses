988584a58db80b378b33486fde6c259cacc77180
Miskachev Dima <miskachevds@yandex.ru>
2026-02-10 13:16:38 +0300

    fetch useEffect

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 
+import React from 'react';
 import './scss/app.scss'
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-// import { useState } from 'react';
-import React from 'react';
 
-import pizzas from './assets/pizzas.json';
+// import pizzas from './assets/pizzas.json';
 // console.log(pizzas)
 
 function App() {
-  const [items,setItems] = useState([]);//изначально пустые данные,
+  const [items, setItems] = React.useState([]);//изначально пустые данные,
 
-  React.useEffect();
-
-  fetch('https://68dd22fe7cd1948060ac902b.mockapi.io/items')
-    .then(response => {//когда отправили запрос,тогда ждем
-      return response.json();//возвращаем и преобразовываем в json
-    })
-    .then((data) => {//тогда,сохраняем всю инф в дата
-      setItems(data);//потом меняем состояние и подгружаем сет,состояние поменялось идет перезагрузка и снова запрос данных поэтому необходимо запр один раз юз эффектом
-      // console.log(data)
-    })
+  React.useEffect(() => {//не получает значение,получает функцию,кот вызывает когда произойдет какой то эффект
+    fetch('https://68dd22fe7cd1948060ac902b.mockapi.io/items')
+      .then(response =>{//когда отправили запрос,тогда ждем
+        return response.json();//возвращаем и преобразовываем в json
+      })
+      .then((data) => {//тогда,сохраняем всю инф в дата
+        setItems(data);//потом меняем состояние и подгружаем сет,состояние поменялось идет перезагрузка и снова запрос данных поэтому необходимо запр один раз юз эффектом
+        // console.log(data)
+      })
+  },[]);//пустой массив=вызвать один раз
 
   return (
     <div className="wrapper">
